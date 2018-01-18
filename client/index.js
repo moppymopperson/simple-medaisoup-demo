d5dfb3a8fef4adc733170ac738bfab7425767d7c
Erik Hornberger <hornbergererik@gmail.com>
2018-01-18 22:11:44 +0900

    All 0s

--- client/index.js
+++ client/index.js
@@ -2,7 +2,9 @@
 
 const io = require('socket.io-client')
 const mediasoupClient = require('mediasoup-client')
+const room = new mediasoupClient.Room();
 const socket = io('http://localhost:3000')
+let receiveTransport
 
 socket.on('connect', () => {
   console.log('Connected!')
@@ -19,17 +21,24 @@ socket.on('notify', notification => {
 
 room.on('request', (request, success) => {
   console.log('Request: ' + request.method)
+  const username = document.getElementById('username').value
+  request.peerName = username
   socket.emit('request', request, success)
 })
 
 room.on('notify', notification => {
+  console.log('Room sending notification', notification)
+  const username = document.getElementById('username').value
+  notification.peerName = username
   socket.emit('notify', notification)
 })
 room.on('newpeer', peer => {
+  console.log('New peer joined room: ' + peer.peerName)
   handlePeer(peer)
 })
 
 const handlePeer = peer => {
+  console.log(`handling peer: ${peer.name}`)
   peer.on('notify', notification => {
     notification.peerName = peer.name
     socket.emit('notify', notification)
@@ -43,16 +52,39 @@ const handlePeer = peer => {
 }
 
 const handleConsumer = consumer => {
-  console.log('Receiving consumer')
-  const stream = new MediaStream()
-  consumer.receive(transport).then(stream.addTrack)
+  console.log('    handling consumer')
+  consumer.receive(receiveTransport).then(track => {
+    const stream = new MediaStream()
+    stream.addTrack(track)
+
+    const AudioContext = window.AudioContext || window.webkitAudioContext
+    const context = new AudioContext()
+    const source = context.createMediaStreamSource(stream)
+    const processor = context.createScriptProcessor(1024, 1, 1)
+
+    let count = 0
+    source.connect(processor)
+    processor.connect(context.destination)
+    processor.onaudioprocess = event => {
+      if (count % 100 === 0) {
+        const raw = event.inputBuffer.getChannelData(0)
+        console.log(raw)
+      }
+      count += 1
+    }
+  })
 }
 
 const startMic = () => {
+  console.log('Starting mic...')
   const transport = room.createTransport('send')
-  navigator.getUserMedia({ audio: true, video: false }).then(stream => {
+  navigator.mediaDevices.getUserMedia({
+    audio: true, video: false
+  }).then(stream => {
     const track = stream.getAudioTracks()[0]
+    console.log('Using audio device: ' + track.label);
     room.createProducer(track).send(transport)
+    track.stop()
   })
 }
 
@@ -61,11 +93,10 @@ global.joinRoom = () => {
   console.log(`Joining room as ${username}!`)
   room.join(username)
     .then(peers => {
-      console.log('Successfully joined room!')
-      //receiveTransport = room.createTransport('recv')
-      for (const peer of peers) {
-        handlePeer(peer);
-      }
+      console.log('Joined!')
+      receiveTransport = room.createTransport('recv')
+      peers.forEach(handlePeer)
+      startMic()
     });
 }
 
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+process.env.DEBUG = "mediasoup"
+
 const fs = require('fs')
 const http = require('http')
 const express = require('express')
@@ -5,125 +7,68 @@ const app = express()
 const mediasoup = require('mediasoup')
 const socketio = require('socket.io')
 
-// Filepaths to SSL certificates for HTTPS
-// const ssl = {
-//   key: fs.readFileSync('./cert/server.key').toString(),
-//   cert: fs.readFileSync('./cert/server.crt').toString()
-// }
-
 // Create an HTTPS server
 const webServer = http.createServer(app).listen(3000, () => {
-  console.log('Web server start running on port 3000')
+    console.log('Web server start running on port 3000')
 })
 
 // Create a socket.io server
 const io = socketio(webServer)
 
 // Create an Selective Forwarding Unit (SFU) server
-const soupServer = mediasoup.Server()
+const soupServer = mediasoup.Server({ logLevel: "debug" })
 const mediaCodecs = [
-  {
-    kind: 'audio',
-    name: 'opus',
-    clockRate: 48000,
-    channels: 2,
-    parameters: {
-      useinbandfec: 1
+    {
+        kind: 'audio',
+        name: 'opus',
+        clockRate: 48000,
+        channels: 2,
+        parameters: {
+            useinbandfec: 1
+        }
     }
-  }
 ]
 const room = soupServer.Room(mediaCodecs)
 
-io.on('connection', (client, message) => {
-  console.log(`Client with id ${client.id} connected!`)
+const notify = notification => {
+    console.log('Notification!', notification)
+    const peerName = notification.peerName
+    room.getPeerByName(peerName).receiveNotification(notification)
+}
 
-  client.on('message', message => {
-    // Initial contact comes from browser as an offer
-    if (message.type === 'mediasoup-request') {
-      const request = message.body
-      console.log(`Received ${request.method} request from client!`)
-
-      if (request.method === 'queryRoom') {
-        room
-          .receiveRequest(request)
-          .then(response => {
-            console.log(`Sending response to ${request.method} request!`)
-            client.send({ type: 'mediasoup-response', body: response })
-          })
-          .catch(e => {
-            console.log('Error receiving request!', e)
-          })
-      } else if (request.method == 'join') {
-        room
-          .receiveRequest(request)
-          .then(response => {
-            client.send({ type: 'mediasoup-response', body: response })
-
-            const { peerName } = request
-            const peer = room.getPeerByName(peerName)
-            handleNewPeer(peer)
-          })
-          .catch(e => {
-            console.log('Error receiving request!', e)
-          })
-      } else if (request.method === 'createTransport') {
-        console.log('Received request to creat a new transport!')
-        room.peers.forEach(peer => {
-          peer.on('newtransport', transport => {
-            console.log('created new transport!')
-          })
-          peer
-            .receiveRequest(request)
-            .then(response => {
-              console.log('Created new transport response')
-              client.send({ type: 'mediasoup-response', body: response })
-            })
-            .catch(error => {
-              console.log('Error creating transport', error)
-            })
-        })
-      } else if (request.method === 'createProducer') {
-        console.log('Received request to create new producer!', request)
-        room.peers.forEach(peer => {
-          console.log('Creating producer for ' + peer.name)
-          peer.on('newproducer', producer => {
-            console.log('New producer created!')
-          })
-          peer.on('newconsumer', consumer => {
-            console.log('New consumer created!')
-          })
-          peer.receiveRequest(request).then(response => {
-            client.send({ type: 'mediasoup-response', body: response })
-          })
-        })
-      } else if (request.method === 'enableConsumer') {
-        console.log('Received request to enable consumer!')
-        room.peers.forEach(peer => {
-          peer
-            .receiveRequest(request)
-            .then(response => {
-              console.log('Enabled consumer!', response)
-              client.send({ type: 'mediasoup-response', body: response })
-            })
-            .catch(error => {
-              console.log('Error receiving request', error)
-            })
-        })
-      } else {
-        fatal('Uknown request type!', request.method)
-      }
+const request = (req, callback) => {
+    console.log(`Received ${req.method} request`)
+    switch (req.target) {
+        case 'room':
+            requestRoom(req, callback)
+            break;
+        case 'peer':
+            requestPeer(req, callback)
+            break;
     }
-  })
+}
 
-  client.on('disconnect', () => {
-    console.log(`Client with id ${client.id} disconnected`)
-  })
+const requestPeer = (request, callback) => {
+    console.log('  Forwarding request to peer: ' + request.peerName)
+    const peerName = request.peerName
+    room.getPeerByName(peerName)
+        .receiveRequest(request)
+        .then(callback)
+}
 
-  client.on('error', error => {
-    console.log(`Client error: ${error}`)
-  })
+const requestRoom = (request, callback) => {
+    console.log('  Forwarding request to the room')
+    room.receiveRequest(request)
+        .then(callback).catch(e => {
+            console.log(e)
+        })
+}
+
+io.on('connection', socket => {
+    console.log('New connection')
+    socket.on('notify', notify)
+    socket.on('request', request)
 })
 
-function handleNewPeer(peer) {
-  console.log(`${peer.name} joined the room!`)
-}
+
+
