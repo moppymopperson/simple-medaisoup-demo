a82a4352a25b7e78108a7cf3b0c0657564019a5a
Erik Hornberger <hornbergererik@gmail.com>
2018-01-18 16:43:49 +0900

    not working

--- client/index.js
+++ client/index.js
@@ -2,115 +2,70 @@
 
 const io = require('socket.io-client')
 const mediasoupClient = require('mediasoup-client')
-
 const socket = io('http://localhost:3000')
-const mediaStream = new MediaStream()
-let receiveTransport
-let sendTransport
 
 socket.on('connect', () => {
   console.log('Connected!')
 })
-
+socket.on('disconnect', () => {
+  console.log('Disconnected!')
+})
 socket.on('error', () => {
   console.log(error)
 })
+socket.on('notify', notification => {
+  room.receiveNotification(notification)
+})
 
-const createRoom = () => {
-  const options = {
-    requestTimeout: 10000,
-    transportOptions: {
-      tcp: false
-    }
-  }
+room.on('request', (request, success) => {
+  console.log('Request: ' + request.method)
+  socket.emit('request', request, success)
+})
 
-  const room = new mediasoupClient.Room(options)
+room.on('notify', notification => {
+  socket.emit('notify', notification)
+})
+room.on('newpeer', peer => {
+  handlePeer(peer)
+})
 
-  room.on('newpeer', handleNewPeer)
-  room.on('notify', handleNotify)
-  room.on('request', dispatchRequest)
-  room.on('error', handleRoomError)
-  return room
-}
-
-global.joinRoom = () => {
-  const room = createRoom()
-  const username = document.getElementById('username').value
-  console.log(`Joining room as ${username}!`)
-  room.join(username).then(peers => {
-    console.log('Creating send and receive transports!')
-    receiveTransport = room.createTransport('recv')
-    sendTransport = room.createTransport('send')
-    startMicrophone(room)
-    peers.forEach(handleNewPeer)
+const handlePeer = peer => {
+  peer.on('notify', notification => {
+    notification.peerName = peer.name
+    socket.emit('notify', notification)
   })
-}
-
-const handleNewPeer = peer => {
-  console.log(`Peer ${peer.name} joined the room!`)
-  peer.consumers.forEach(handleConsumer)
+  peer.on('request', (request, answer) => {
+    request.peerName = peer.name
+    socket.emit('request', request, answer)
+  })
   peer.on('newconsumer', handleConsumer)
+  peer.consumers.forEach(handleConsumer)
 }
 
 const handleConsumer = consumer => {
-  console.log(`Adding new consumer for peer ${consumer.peer.name}!`)
-  consumer.receive(receiveTransport).then(track => {
-    console.log('Track enabled!')
-    mediaStream.addTrack(track)
+  console.log('Receiving consumer')
+  const stream = new MediaStream()
+  consumer.receive(transport).then(stream.addTrack)
+}
 
-    const context = new AudioContext()
-    const source = context.createMediaStreamSource(mediaStream)
-    const processor = context.createScriptProcessor(1024, 1, 1)
-
-    let count = 0
-    source.connect(processor)
-    processor.onaudioprocess = event => {
-      console.log('event!')
-      //   if (count % 100 === 0) {
-      //     const data = event.inputBuffer.getChannelData(0)
-      //     console.log(data)
-      //   }
-      //   count += 1
-    }
+const startMic = () => {
+  const transport = room.createTransport('send')
+  navigator.getUserMedia({ audio: true, video: false }).then(stream => {
+    const track = stream.getAudioTracks()[0]
+    room.createProducer(track).send(transport)
   })
 }
 
-const handleNotify = notification => {
-  console.log('handleNotify')
-  fatal('Not implemented')
+global.joinRoom = () => {
+  const username = document.getElementById('username').value
+  console.log(`Joining room as ${username}!`)
+  room.join(username)
+    .then(peers => {
+      console.log('Successfully joined room!')
+      //receiveTransport = room.createTransport('recv')
+      for (const peer of peers) {
+        handlePeer(peer);
+      }
+    });
 }
 
-const handleRoomError = error => {
-  console.log('Room Error!', error)
-}
-
-const startMicrophone = room => {
-  console.log('Starting microphone...')
-  navigator.mediaDevices
-    .getUserMedia({ audio: true, video: false })
-    .then(stream => {
-      const audio = stream.getAudioTracks()[0]
-      const producer = room.createProducer(audio)
-      producer.send(sendTransport)
-    })
-    .then(() => {
-      console.log('Sending audio!')
-    })
-    .catch(handleRoomError)
-}
-
-const dispatchRequest = (request, accept, reject) => {
-  socket.on('message', message => {
-    console.log(`Received response to ${request.method} request`)
-    if (message.type === 'mediasoup-response') {
-      const response = message.body
-      accept(response)
-      console.log('Accepted response from server!')
-    } else {
-      console.warn('Unknown message type!')
-    }
-  })
-
-  console.log(`Dispatching ${request.method} request`)
-  socket.send({ type: 'mediasoup-request', body: request })
-}
